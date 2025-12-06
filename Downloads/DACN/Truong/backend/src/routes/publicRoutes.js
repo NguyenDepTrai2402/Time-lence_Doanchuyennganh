66d8413e5710befb59281939e982b49885377029
Nguyễn Trọng Nguyễn <modecaiandriborn@gmail.com>
2025-12-07 02:13:55 +0700

    Delete Downloads/DACN/Truong directory

--- Downloads/DACN/Truong/backend/src/routes/publicRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const scheduleController = require('../controllers/scheduleController');
-
-// Public routes (no authentication required)
-// Shared schedule functionality not available in current MySQL schema
-
-module.exports = router;
